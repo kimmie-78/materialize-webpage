0058569f416f5dd6383bbdb8e9c38187c21a553b
KIMMIE-78 <kimberelyisaac217@gmail.com>
2024-05-31 14:27:09 +0200

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import data from './data.json';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
-    plugins: [handlebars({
-      context : data
-    
-
-    })],
-  };
+  base : '/responsive_cv/',   // <-- add this
+  plugins: [handlebars({
+      context : data   // add this
+  })],
+};
